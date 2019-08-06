772088f7dd15b32a117062a11d3569f8efbf83fc
lpuad <lpuad@LAPTOP-845IQLEN>
2019-08-05 20:55:20 -0400

    1st commit

--- BookInventory/src/bookinventory/BookInventory.java
+++ BookInventory/src/bookinventory/BookInventory.java
@@ -5,6 +5,11 @@
  */
 package bookinventory;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.Scanner;
+
 /**
  *
  * @author Brydon
@@ -14,8 +19,20 @@ public class BookInventory{
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
+        
+        try (RandomAccessFile test = new RandomAccessFile("test.dat", "rw");)
+        {
+            test.writeUTF("12345");
+            test.writeUTF("Harry Potter");
+            test.writeUTF("54321");
+            test.writeUTF("Lotr");
+        }
+        
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your book: ");
+        
     }
 
 }
